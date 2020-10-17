0ab8c1da018e864b47fa4a6e3f55bf4095a64db8
Lenny Castaneda <lenny.castaneda@gmail.com>
2020-10-17 16:56:30 -0700

    Add unit test for sending tokens

---
+++ test/helpers.js
@@ -0,0 +1,8 @@
+// Helper converter function
+export const tokens = (n) => {
+  //return new web3.utils.asciiToHex(
+  return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+  )
+}
+  
