5f6316b942a8dd2216916c5d2bffaf87cd2aa11e
Lenny Castaneda <lenny.castaneda@gmail.com>
2020-10-25 12:36:00 -0700

    Set exchange fee account

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -9,14 +9,16 @@ pragma solidity ^0.5.0;
 contract Exchange {
     // Variables
     address public feeAccount; // account the recieves exchange fees
+    uint256 public feePercent; // fee percentage
 
-    constructor(address _feeAccount) public {
+    constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
+        feePercent = _feePercent;
     }
 }
 
 // TODO:
-// [ ] Set the fee account
+// [X] Set the fee account
 // [ ] Deposit Ether
 // [ ] Withdraw Ether
 // [ ] Deposit tokens
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -8,15 +8,20 @@ require('chai')
 
 contract('Exchange', ([deployer, feeAccount]) => {
   let exchange
+  const feePercent = 10
   
   beforeEach(async () => {
-    exchange = await Exchange.new(feeAccount)
+    exchange = await Exchange.new(feeAccount, feePercent)
   })
 
   describe('deployment', () => {
     it('tracks the fee account', async () => {
       const result = await exchange.feeAccount()
       result.should.equal(feeAccount)
+    })
+    it('tracks the fee percent', async () => {
+      const result = await exchange.feePercent()
+      result.toString().should.equal(feePercent.toString())
     })
   })
 })
