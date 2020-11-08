08bc6bb6c62d912299df2b2c0ad6920a0d2fed45
Lenny Castaneda <lenny.castaneda@gmail.com>
2020-11-07 17:25:34 -0800

    Added to Exchange smart contract: balanceOf, withdrawToken & withdrawEther with unit tests

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -30,6 +30,12 @@ contract Exchange {
 
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
 
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
@@ -46,6 +52,16 @@ contract Exchange {
         emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
     }
 
+    function withdrawEther(uint256 _amount) public {
+        // Check for sufficient balance to send tokens; else stop execution
+        require(tokens[ETHER][msg.sender] >= _amount);
+
+        tokens[ETHER][msg.sender] -= _amount;
+        msg.sender.transfer(_amount);
+
+        emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
+    }
+
     function depositToken(address _token, uint256 _amount) public {
         // Don't allow Ether deposits
         require(_token != ETHER);
@@ -58,5 +74,28 @@ contract Exchange {
 
         // Emit event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        // Ensure this is not an Ether address
+        require(_token != ETHER);
+
+        // Make sure there is enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
+
+        tokens[_token][msg.sender] -= _amount;
+
+        // Require token gets transferred back to user from this SmartContract
+        require(Token(_token).transfer(msg.sender, _amount));
+
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
     }
 }
