9811a196bce163eedca5adfbdd8c618f6190f0dd
Lenny Castaneda <lenny.castaneda@gmail.com>
2021-05-15 15:39:44 -0700

    More Redux configuration for development

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import rootReducer from "./reducers";
 
+const loggerMiddleware = createLogger()
+const middleware = []
+
+// For Redux Dev Tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 // Create a Redux store holding the state of your app.
 export default function configureStore(reloadedState) {
   return createStore(
     rootReducer,
-    preloadedState
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middleware, loggerMiddleware))
   )
 }
