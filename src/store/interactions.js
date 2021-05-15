e4a88d828acedf7e0588c00217babee75f9fd9fd
Lenny Castaneda <lenny.castaneda@gmail.com>
2021-05-15 16:07:54 -0700

    Added comments

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -5,6 +5,9 @@ import {
 // Trigger an action and dispatch it with Redux, dispatch will be injected into reducer to update state
 export const loadWeb3 = (dispatch) => {
   const web3 = new Web3(window.ethereum)
+
+  // Send this action: 'web3Loaded' to Redux
   dispatch(web3Loaded(web3))
+  
   return web3
 }
