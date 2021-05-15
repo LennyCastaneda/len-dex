51d0bb21433e6a90ef058ead3c4565fe76dc29c9
Lenny Castaneda <lenny.castaneda@gmail.com>
2021-05-15 15:36:15 -0700

    Added preloadedState argument to configureStore function

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,8 +2,9 @@ import { createStore } from 'redux'
 import rootReducer from "./reducers";
 
 // Create a Redux store holding the state of your app.
-export default function configureStore() {
+export default function configureStore(reloadedState) {
   return createStore(
-    rootReducer
+    rootReducer,
+    preloadedState
   )
 }
