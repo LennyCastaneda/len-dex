2973b3404be721f246d31f3dc1e61829f503687a
Lenny Castaneda <lenny.castaneda@gmail.com>
2021-05-15 15:57:07 -0700

    Initiazlied Redux Dev Tools by updating index.js with provider calling configureStore

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ import * as serviceWorker from './serviceWorker'
 
 const store = configureStore()
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
+ReactDOM.render(
+  
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
