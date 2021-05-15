4cb6beef31b56c31a8d3600f29b163656a47d101
Lenny Castaneda <lenny.castaneda@gmail.com>
2021-05-15 15:35:23 -0700

    Cleaned up createStore() and configureStore() by consolidating into the same function

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,8 @@ import { createStore } from 'redux'
 import rootReducer from "./reducers";
 
 // Create a Redux store holding the state of your app.
-const store = createStore(
-  rootReducer
-)
-
 export default function configureStore() {
-  return store
+  return createStore(
+    rootReducer
+  )
 }
